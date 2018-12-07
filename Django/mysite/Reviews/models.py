b62126ccfa569b45c8c5146f9ff2716e164da29c
TinkerTavern <43844772+TinkerTavern@users.noreply.github.com>
2018-12-07 19:41:36 +0000

    Added models to models.py

--- Django/mysite/Reviews/models.py
+++ Django/mysite/Reviews/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Review(models.Model):
-    reviewText = models.CharField(max_length=200)
-    location = models.CharField(default="",max_length=200)
-    author = models.CharField('Author',max_length=200)
+    reviewText = models.TextField()
+    location = models.CharField(max_length=50)
+    author = models.CharField('Author',max_length=50)
+    upvotes = models.IntegerField(default=0)
+    datePosted = models.DateField(auto_now=True)
--- Django/mysite/Reviews/views.py
+++ Django/mysite/Reviews/views.py
@@ -6,18 +6,18 @@ from django.core import serializers
 
 # Create your views here.
 
-def index(request, question_id):
-    return HttpResponse("You're looking at index %s" % question_id)
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+# def index(request, question_id):
+#     return HttpResponse("You're looking at index %s" % question_id)
+#
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+#
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+#
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
 
 def addReviewPage(request):
     context = {}
